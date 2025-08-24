26d586546268363bf8af380aef211737764c48fd
Sangameshwar-007 <sangameshwarkama@gmail.com>
2025-08-24 23:47:58 +0530

    todo final app

--- todo_main/views.py
+++ todo_main/views.py
@@ -11,9 +11,14 @@ from todos.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
-    # print(tasks)  # This will still show <QuerySet ...> in terminal
+
+    completed_tasks = Task.objects.filter(is_completed=True)
+
+
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
+
     }
 
     return render(request, 'home.html', context)
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 
@@ -12,3 +12,30 @@ def addTask(request):
 
     return redirect('home')
 
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task_name = request.POST.get('task')
+        if task_name:
+            task.task = task_name
+            task.save()
+        return redirect('home')
+    return render(request, 'edit_task.html', {'task': task})
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
